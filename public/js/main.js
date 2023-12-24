4faf44acdb9fb6c3ddb3247d069bf24e07b3baa7
Kevins Villatoro <kevinnz36@gmail.com>
2023-12-23 21:28:21 -0300

    Modificaciones para funcionamiento completo en mobile

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const homeLink = document.getElementById('home-link');
     const contentLink = document.getElementById('content-link');
     const contactLink = document.getElementById('contact-link');
+    const homeLinkMobile = document.getElementById('home-link-mobile');
+    const contentLinkMobile = document.getElementById('content-link-mobile');
+    const contactLinkMobile = document.getElementById('contact-link-mobile');
     const homeSection = document.getElementById('home-section');
     const contentSection = document.getElementById('content-section');
     const contactSection = document.getElementById('contact-section');
@@ -31,7 +34,29 @@ document.addEventListener('DOMContentLoaded', function () {
         contactSection.style.display = 'block';
     }
 
+    function showHomeSectionMobile(event) {
+        event.preventDefault();
+        hideAllSections();
+        homeSection.style.display = 'block';
+    }
+    
+    function showContentSectionMobile(event) {
+        event.preventDefault();
+        hideAllSections();
+        contentSection.style.display = 'block';
+    }
+    
+    function showContactSectionMobile(event) {
+        event.preventDefault();
+        hideAllSections();
+        contactSection.style.display = 'block';
+    }
+
     homeLink.addEventListener('click', showHomeSection);
     contentLink.addEventListener('click', showContentSection);
-    contactLink.addEventListener('click', showContactSection)
+    contactLink.addEventListener('click', showContactSection);
+
+    homeLinkMobile.addEventListener('click', showHomeSectionMobile);
+    contentLinkMobile.addEventListener('click', showContentSectionMobile);
+    contactLinkMobile.addEventListener('click', showContactSectionMobile);
 });
